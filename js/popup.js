28006335a9a331f44f37e6e4a3cb8ff4a233acb6
harsh472000 <harshmeghani777@gmail.com>
2021-10-28 09:34:39 +0530

    add items page added

---
+++ js/popup.js
@@ -0,0 +1,9 @@
+document.querySelector(".show-aitem")
+.addEventListener("click",function(){
+    document.querySelector(".popupmodel").classList.add("active");
+});
+
+document.querySelector(".popupmodel .close-btn")
+.addEventListener("click",function(){
+    document.querySelector(".popupmodel").classList.remove("active");
+});
